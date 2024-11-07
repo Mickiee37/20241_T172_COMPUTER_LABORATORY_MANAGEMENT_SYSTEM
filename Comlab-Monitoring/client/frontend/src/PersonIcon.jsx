ead539c97811e9a4913be5c221a2d6445de9f6eb
Michael Kate B. Esparcia <2201102054@student.buksu.edu.ph>
2024-11-08 05:50:26 +0800

    Added email validation in register

--- Comlab-Monitoring/client/frontend/src/PersonIcon.jsx
+++ Comlab-Monitoring/client/frontend/src/PersonIcon.jsx
@@ -1,7 +1,7 @@
 // PersonIcon.jsx
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import { FaUser } from 'react-icons/fa'; // Import the user icon from react-icons
+import { Link } from 'react-router-dom'; 
+import { FaUser } from 'react-icons/fa';
 
 const PersonIcon = () => {
   return (
--- Comlab-Monitoring/client/frontend/src/main.jsx
+++ Comlab-Monitoring/client/frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App.jsx';
 import Login from './Login.jsx'; 
@@ -11,7 +12,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/app" element={<App />} />
