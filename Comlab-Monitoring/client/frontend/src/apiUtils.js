57f171c08e9ef373faf0257a1993ab406a1f62b3
Michael Kate B. Esparcia <2201102054@student.buksu.edu.ph>
2024-12-18 00:01:49 +0800

    Added instructor validation

--- Comlab-Monitoring/client/frontend/src/apiUtils.js
+++ Comlab-Monitoring/client/frontend/src/apiUtils.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Open Lab by scanning Lab QR Code
 export const scanLabQRCode = async (labNumber) => {
   try {
-    const response = await axios.post('http://localhost:8000/api/labs/scan-lab', { labNumber });
+    const response = await axios.post('http://192.168.194.244:8000/api/labs/scan-lab', { labNumber });
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data.message || 'Error scanning Lab QR Code');
@@ -13,7 +13,7 @@ export const scanLabQRCode = async (labNumber) => {
 // Assign Instructor by scanning Instructor QR Code
 export const scanInstructorQRCode = async (labNumber, instructorName) => {
   try {
-    const response = await axios.post('http://localhost:8000/api/labs/scan-instructor', {
+    const response = await axios.post('http://192.168.194.244:8000/api/labs/scan-instructor', {
       labNumber,
       instructorName,
     });
